47df897a2002c2fe3377fbf6e54a36fe71ecb843
costinelnegura <costinel.public@icloud.com>
2022-09-17 16:53:32 +0100

    update NotionConfig with description

--- src/main/java/com/costinel/notionGetPage/notion/config/NotionConfig.java
+++ src/main/java/com/costinel/notionGetPage/notion/config/NotionConfig.java
@@ -8,20 +8,19 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class NotionConfig {
 
+    /*
+    Increasing the maxInMemorySize to receive a larger Json from Notion.
+    The following is needed in application.properties as well:
+    spring.codec.max-in-memory-size=2MB
+     */
+
     @Bean
-    public WebClient getWebClientBuilder(){
-
-        return   WebClient.builder().exchangeStrategies(ExchangeStrategies.builder()
-
+    public WebClient getWebClientBuilder() {
+        return WebClient.builder().exchangeStrategies(ExchangeStrategies.builder()
                         .codecs(configurer -> configurer
-
                                 .defaultCodecs()
-
                                 .maxInMemorySize(16 * 1024 * 1024))
-
                         .build())
-
                 .build();
-
     }
 }
