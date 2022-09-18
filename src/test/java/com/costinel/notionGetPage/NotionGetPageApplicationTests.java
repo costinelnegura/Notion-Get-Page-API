268cd158ee8c03c46e13aa987989d840fb6827ab
costinelnegura <costinel.public@icloud.com>
2022-09-18 14:02:45 +0100

    initial test

--- src/test/java/com/costinel/notionGetPage/NotionGetPageApplicationTests.java
+++ src/test/java/com/costinel/notionGetPage/NotionGetPageApplicationTests.java
@@ -1,13 +1,44 @@
 package com.costinel.notionGetPage;
 
+import com.costinel.notionGetPage.model.Email;
+import com.costinel.notionGetPage.notion.NotionClient;
+import com.costinel.notionGetPage.notion.config.NotionConfigProperties;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @SpringBootTest
 class NotionGetPageApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    NotionClient notionClient;
 
+    @Autowired
+    NotionConfigProperties notionConfigProperties;
+
+    /*
+    Run this test after you declare the following as environment variables in your IDE!!!
+    notion.database-id=<your notion database ID>
+    notion.secret-token=<your notion secret token>
+     */
+    @Test
+    public void itShouldReturnStatus200_OK() {
+        //given
+        List<Email> expectedEmailList = new ArrayList<>();
+        ResponseEntity<List<Email>> expectedResult = new ResponseEntity<>(expectedEmailList, HttpStatus.OK);
+
+        //when
+        ResponseEntity<List<Email>> listResponseEntity =
+                notionClient.databases.queryDatabase(notionConfigProperties.databaseId());
+
+        //then
+        Assertions.assertEquals(expectedResult.getStatusCode(), listResponseEntity.getStatusCode());
+    }
 }
